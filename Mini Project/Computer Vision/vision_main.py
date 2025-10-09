f5a1b79188aaf43af466e17a1a0af7ea22e16bd0
jdgomana <149437147+jdgomana@users.noreply.github.com>
2025-10-08 21:11:29 -0600

    Update vision_main.py

--- Mini Project/Computer Vision/vision_main.py
+++ Mini Project/Computer Vision/vision_main.py
@@ -1,8 +1,7 @@
 '''
-This program detects an Aruco marker in the camera feed and identifies which quadrant of
-the frame it is located in.
-The quadrant corresponds to a string value that represents wheel positions, which will
-be sent to the LCD screen and Arduino over I2C on position updates.
+This program detects an Aruco marker in the camera feed and identifies which quadrant of the frame it is located in.
+The quadrant corresponds to a string value that represents wheel positions, which will be sent to the LCD screen and Arduino over I2C on 
+position updates.
 The LCD logic utilizes threading to display the goal position without lag.
 '''
 
