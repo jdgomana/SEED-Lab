afb15acdcedb371fc6b1102234b7a0853ec8856e
jdgomana <149437147+jdgomana@users.noreply.github.com>
2025-11-17 22:14:39 -0700

    Add comments for clarity on movement commands
    Added comments explaining the program's functionality and FSM.

--- Demo 2/CV/trial2.py
+++ Demo 2/CV/trial2.py
@@ -1,3 +1,6 @@
+# This program uses distance and angle calculations to send movement commands to the Arduino. Arrow detection is also added.
+# The FSM proceeds as follows: calculates angle to ArUco marker until close to 0 -> calculates distance to marker until 1.5ft -> calculates angle again until closer to zero -> detect direction of arrow -> end
+
 from smbus2 import SMBus
 from time import sleep
 import numpy as np
